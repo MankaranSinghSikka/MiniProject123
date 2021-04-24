35b1bd3c7e72b3d7ce41867b117aab88cd341783
MankaranSinghSikka <mankaran.sikka@gmail.com>
2021-04-24 12:47:17 +0530

    UI changes

---
+++ SharingEconomyPlatform/SharingEconomyPlatform/Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using SharingEconomyPlatform.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SharingEconomyPlatform.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.ToList();
+
+            return View();
+        }
+    }
+}
---
+++ SharingEconomyPlatform/SharingEconomyPlatform/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SharingEconomyPlatform.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+    }
+}
